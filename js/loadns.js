114943e07ca8749b27240dee14ac135c4d54d87a
Arcker89-cyber <customerarcker@gmail.com>
2025-12-22 21:04:17 +0700

    Update loadns.js

--- js/loadns.js
+++ js/loadns.js
@@ -1,23 +1,13 @@
-auth.onAuthStateChanged(user => {
-    if (!user) return;
-  
-    db.collection("loans").onSnapshot(snapshot => {
-      const tb = document.getElementById("loanTable");
-      tb.innerHTML = "";
-      let i = 1;
-  
-      snapshot.forEach(doc => {
-        const d = doc.data();
-        tb.innerHTML += `
-          <tr>
-            <td>${i++}</td>
-            <td>${d.fullname}</td>
-            <td>${d.principal}</td>
-            <td>${d.interest}</td>
-            <td>${d.status}</td>
-          </tr>
-        `;
-      });
-    });
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "index.html";
+  } else {
+    console.log("Logged in as", user.email);
+  }
+});
+
+function logout() {
+  firebase.auth().signOut().then(() => {
+    window.location.href = "index.html";
   });
-  
+}
