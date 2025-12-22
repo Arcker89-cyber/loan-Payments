15e252a71d716aa291976728a81031e122185b1e
Arcker89-cyber <customerarcker@gmail.com>
2025-12-22 20:45:43 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,24 +1,13 @@
 function login() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-  
-    auth.signInWithEmailAndPassword(email, password)
-      .then(() => {
-        window.location.href = "dashboard.html";
-      })
-      .catch(err => alert(err.message));
-  }
-  
-  function logout() {
-    auth.signOut().then(() => {
-      window.location.href = "index.html";
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(err => {
+      alert(err.message);
+      console.error(err);
     });
-  }
-  
-  // ป้องกันเข้า dashboard โดยไม่ login
-  auth.onAuthStateChanged(user => {
-    if (!user && location.pathname.includes("dashboard")) {
-      window.location.href = "index.html";
-    }
-  });
-  
+}
