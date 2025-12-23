b90bb2f04aaeb693458478feb804707ae9d94a70
Arcker89-cyber <customerarcker@gmail.com>
2025-12-23 09:05:25 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,13 @@
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCXtDqWSxlFxkHjXmbG3V7P3-niJmV47yA",
   authDomain: "loan-payments-a302b.firebaseapp.com",
   projectId: "loan-payments-a302b",
   storageBucket: "loan-payments-a302b.appspot.com",
   messagingSenderId: "989223042934",
-  appId: "1:989223042934:web:5f13e17eadea1000b88457",
-  measurementId: "G-TG1941VPKD"
+  appId: "1:989223042934:web:5f13e17eadea1000b88457"
 };
 
-// Initialize Firebase (compat)
 firebase.initializeApp(firebaseConfig);
 
-// Global objects
 const auth = firebase.auth();
 const db = firebase.firestore();
