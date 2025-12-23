881cf3d99f7bda284b986c5db46a880391d3b0c0
Arcker89-cyber <customerarcker@gmail.com>
2025-12-23 09:05:51 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,13 +1,16 @@
 function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  const errorMsg = document.getElementById("errorMsg");
+
+  errorMsg.textContent = "";
 
   auth.signInWithEmailAndPassword(email, password)
     .then(() => {
       window.location.href = "dashboard.html";
     })
     .catch(err => {
-      alert(err.message);
+      errorMsg.textContent = err.message;
       console.error(err);
     });
 }
